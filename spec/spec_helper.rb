1330634b7a537abcdf6650be732a8479d8937009
Pete Poggione <pete@poggione.org>
2011-12-19 11:00:06 -0500

    Removed default Rails page

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However, 
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
-  
+
 end
 
 Spork.each_run do
@@ -24,13 +24,10 @@ end
 #   feel free to delete them.
 #
 
-
-
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
